b19be4c4f7c511ecfafa7249e6a850adaa7aa2fb
abdul1ah33 <study.abdullah2005@gmail.com>
2025-09-09 17:26:03 +0300

    added print_int fn to print_parameters_FNs.c

--- main.h
+++ main.h
@@ -3,10 +3,13 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
 
 int _putchar(char c);
 int _strlen(char* str);
 int print_char(char c, int printed_counter);
+int print_int(int num, int printed_counter);
 int _printf(const char *format, ...);
 
 #endif
--- print_parameters_FNs.c
+++ print_parameters_FNs.c
@@ -21,11 +21,41 @@ int print_str(char* str, int printed_counter)
 
 int print_int(int num, int printed_counter)
 {
-    int i;
+    int i = 0;
+    char* str = "";
+    int negative = 0;
+
     if (num == 0)
-        _putchar(num);
-    while (num)
     {
-        
+        str = '0';
+        _putchar(str);
     }
+
+    if (num < 0)
+    {
+        negative = 1;
+        _putchar('-');
+    }
+
+    while (num != 0)
+    {
+        str[i] = num % 10;
+        num = num / 10;
+        i++;
+    }
+
+    i--;
+
+    while (i >= 0)
+    {
+        _putchar(str[i]);
+        i--;
+    }
+
+    if (negative == 1)
+        printed_counter += _strlen(str) + 1;
+    else
+        printed_counter += _strlen(str);
+
+    return printed_counter;
 }
