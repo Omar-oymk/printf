1cbb30c750a4e25fe21f4db5c33313ced6b5572f
abdul1ah33 <study.abdullah2005@gmail.com>
2025-09-09 16:51:40 +0300

    added print_char fn to print_parameters_FNs.c

---
+++ print_parameters_FNs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+int print_char(char c, int printed_counter)
+{
+    _putchar(c);
+    printed_counter++;
+    return printed_counter;
+}
+
+int print_str(char* str, int printed_counter)
+{
+    
+}
