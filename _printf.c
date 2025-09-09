f0351dfcfbc1d2a580a313154b18cfceda04e475
abdul1ah33 <study.abdullah2005@gmail.com>
2025-09-09 16:35:35 +0300

    add the structure of _printf fn

--- _printf.c
+++ _printf.c
@@ -4,5 +4,33 @@
 
 int _printf(const char *format, ...)
 {
+    int Print_counter = 0, i = 0;
 
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            if (format[i+1] == 'd')
+            {
+
+            }
+            else if (format[i+1] == 'c')
+            {
+
+            }
+            else if (format[i+1] == 'c')
+            {
+                
+            }
+        }
+        else if (format[i] == '\\')
+        {
+
+        }
+        else
+        {
+            Print_counter++;
+            _putchar(format[i]);
+        }
+    }
 }
